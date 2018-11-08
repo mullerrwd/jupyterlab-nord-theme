c2978269d6987d43663ad947982aeba60b3172e5
Robert Muller <robert.muller@tno.nl>
2018-11-08 14:41:22 +0100

    First changes made. Far from finished.

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,27 @@
+// Copyright (c) Jupyter Development Team.
+// Distributed under the terms of the Modified BSD License.
 
-import {
-  JupyterLab, JupyterLabPlugin
-} from '@jupyterlab/application';
+import { JupyterLab, JupyterLabPlugin } from '@jupyterlab/application';
 
-import {
-  IThemeManager
-} from '@jupyterlab/apputils';
-
+import { IThemeManager } from '@jupyterlab/apputils';
 
 /**
- * A plugin for the Nord Theme
+ * A plugin for the Jupyter Light Theme. Modified for nord Theme.
  */
 const plugin: JupyterLabPlugin<void> = {
-  id: 'jupyterlab-nord-theme:plugin',
+  id: '@jupyterlab/jupyterlab-nord-theme:plugin',
   requires: [IThemeManager],
   activate: function(app: JupyterLab, manager: IThemeManager) {
+    const style = '@jupyterlab/jupyterlab-nord-theme/index.css';
+
     manager.register({
       name: 'Nord Theme',
       isLight: true,
-      load: function() {
-        return manager.loadCSS('jupyterlab-nord-theme/index.css');
-      },
-      unload: function() {
-        return Promise.resolve(void 0);
-      }
+      load: () => manager.loadCSS(style),
+      unload: () => Promise.resolve(undefined)
     });
   },
   autoStart: true
 };
-
 
 export default plugin;
